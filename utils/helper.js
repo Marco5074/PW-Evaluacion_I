0b7a2e1a11bf943ba4c68c037817a067e6ad0466
Marco5074 <00114825@uca.edu.sv>
2026-09-17 14:19:50 -0600

    ✨ feat: Agregar validación de prioridad y campos vacíos

--- utils/helper.js
+++ utils/helper.js
@@ -1,1 +1,14 @@
 
+//PrioridadValida 
+
+function validarPrioridad(prioridad)
+{
+    return ['alta' , 'media' , 'baja'].includes(prioridad.toLowerCase()); 
+}
+
+function campoVacio(valor)
+{
+    return !valor || valor.trim() === ''; 
+}
+//prueba
+
